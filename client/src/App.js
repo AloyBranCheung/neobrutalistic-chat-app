79d03494143c615a07be3c9d1eb470d3937324e8
bcheung <brandon.cheung.dev@gmail.com>
2023-10-22 23:12:08 -0400

    maybe don't need auth if showing demo account...

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Home from "./components/pages/Home";
 import { useState } from "react";
 import { io } from "socket.io-client";
 import ChatPage from "./components/pages/ChatPage";
+import PrivateRoute from "./auth/ProtectedRoute";
+import LoginPage from "./components/pages/LoginPage";
 
 // client connects to server that is on port 3001
 const socket = io.connect(process.env.REACT_APP_SERVER_URL, {
@@ -17,22 +19,27 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Home
-              username={username}
-              setUsername={setUsername}
-              room={room}
-              setRoom={setRoom}
-              socket={socket}
-            />
-          }
-        />
-        <Route
-          path="/chat"
-          element={<ChatPage username={username} room={room} socket={socket} />}
-        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<PrivateRoute />}>
+          <Route
+            path="/"
+            element={
+              <Home
+                username={username}
+                setUsername={setUsername}
+                room={room}
+                setRoom={setRoom}
+                socket={socket}
+              />
+            }
+          />
+          <Route
+            path="chat"
+            element={
+              <ChatPage username={username} room={room} socket={socket} />
+            }
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/components/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LoginPage() {
+  return (
+    <div className="bg-background flex h-screen items-center justify-center">
+      LoginPage
+    </div>
+  );
+}
